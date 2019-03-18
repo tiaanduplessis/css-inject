7f8f6f10e43198f916033e4d886d000be82e9a25
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2019-03-18 14:13:48 +0200

    Build latest

--- dist/css-inject.es.js
+++ dist/css-inject.es.js
@@ -1,2 +1,2 @@
-var e=document.head,t=e.querySelector('[rel="stylesheet"]');export default function(r){void 0===r&&(r="");var n=document.createElement("style");return n.type="text/css",n.appendChild(document.createTextNode(r)),e.insertBefore(n,t),function(){return e.removeChild(n)}};
+var e=document.head,t=e.querySelector('[rel="stylesheet"]')||e.querySelector("link");export default function(r){void 0===r&&(r="");var n=document.createElement("style");return n.type="text/css",n.appendChild(document.createTextNode(r)),t?e.insertBefore(n,t):e.appendChild(n),function(){return e.removeChild(n)}}
 //# sourceMappingURL=css-inject.es.js.map
